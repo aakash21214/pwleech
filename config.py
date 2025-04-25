bef6fb1f4cba92178bd470a3742b71be8a2b03b9
aakash21214 <93417145+aakash21214@users.noreply.github.com>
2025-04-25 21:43:29 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "27169431"
+api_id = "12916125"
 api_hash = "dfebf9cc52b859771cf8a1d447e751a5"
-bot_token = "b47609fbf8f2c19e9241fd4c7bb43971"
+bot_token = "7547591548:AAEHHIJCJxMHYKjpUiFybdVTDCUC5NJEyng"
 auth_users = [1651746145]
 sudo_users = [6812860793]
