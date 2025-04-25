33f03358270b50fa5792e3c771a56edf527eb5b6
aakash21214 <93417145+aakash21214@users.noreply.github.com>
2025-04-25 21:11:25 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
-api_id = "api id"
-api_hash = "api hadh"
-bot_token = "bot token"
-auth_users = [7890781002]
-sudo_users = [7890781002]
+api_id = "27169431"
+api_hash = "dfebf9cc52b859771cf8a1d447e751a5"
+bot_token = "b47609fbf8f2c19e9241fd4c7bb43971"
+auth_users = [1651746145]
+sudo_users = [6812860793]
